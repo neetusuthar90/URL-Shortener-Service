f57341aee31cdf736c24edcf1b04c918f8048d96
Neetu Kumari <neetusuthar90@gmail.com>
2023-04-09 19:14:23 +0530

    Connect to database

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
-from flask import Flask,request,redirect,jsonify
+from flask import Flask, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import string
 import random
 import os
@@ -8,41 +7,32 @@ import os
 
 app = Flask(__name__)
 
-#app configuration
+# app configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysecretpassword@127.0.0.1/my_database'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#Initialize SQLAlchemy and Migrate
+# Initialize SQLAlchemy and Migrate
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
 
-#define a model
+# define a model
+
 class url_mapping(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    long_url = db.Column(db.String(1024),nullable=False)
-    short_url = db.Column(db.String(62),nullable=False,unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(1024), nullable=False)
+    short_url = db.Column(db.String(62), nullable=False, unique=True)
 
-    def __repr__(self):
-        return f'<url_mapping{self.short_url}>'
+# Run this manually to create schema 
+# from app import db,app 
+# with app.app_context():
+#   db.create_all()
 
 
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return 'Hello, World!'
-
-# if __name__ == '__main__':
-#     app.run()
-
-#store short url in memory
-#url_mapping = {}
-
-#generate short url
+# generate short url
 def generate_short_url(length=7):
     char = string.ascii_letters + string.digits
     short_url = ''.join(random.choice(char) for _ in range(length))
     return short_url
 
-
+## Create short URL and store in DB
 @app.route('/shorten_url', methods=['POST'])
 def shorten_url():
     data = request.get_json()
@@ -51,30 +41,32 @@ def shorten_url():
 
     long_url = data.get('long_url')
     if long_url:
-        short_url = generate_short_url()
-        while url_mapping.query.filter_by(short_url=short_url).first() is not None:
+        record = url_mapping.query.filter_by(long_url=long_url).first()
+        if record is None:
             short_url = generate_short_url()
-        
-        new_url = url_mapping(long_url=long_url,short_url=short_url)
-        db.session.add(new_url)
-        db.session.commit()
-
-        response = {
-            'short_url': f'http://{request.host}/{short_url}'
-        }
-        return jsonify(response),201
+            new_url = url_mapping(long_url=long_url, short_url=short_url)
+            db.session.add(new_url)
+            db.session.commit()
+            response = {'short_url': f'http://{request.host}/{short_url}'}
+        else:
+            response = {
+                'short_url': f'http://{request.host}/{record.short_url}'
+            }
+        return jsonify(response), 201
     else:
-        return jsonify({'error':'Invalid long url provided'}),400
+        return jsonify({'error': 'Invalid long url provided'}), 400
 
 
-@app.route('/<short_url>',methods=['GET'])
+## Redirect to long url
+@app.route('/<short_url>', methods=['GET'])
 def redirect_url(short_url):
-    long_url = url_mapping.query.filter_by(short_url=short_url).first()
-    if not long_url:
-        return jsonify({"error:":"Short url not found"}),404
-    else:
-        return redirect(long_url)
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=os.environ.get('PORT',5000))
+    record = url_mapping.query.filter_by(short_url=short_url).first()
 
+    if not record:
+        return jsonify({"error:": "Short url not found"}), 404
+    else:
+        return redirect(record.long_url)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
